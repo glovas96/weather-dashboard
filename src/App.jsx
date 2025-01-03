a58ba51a7695b2a89a191858253c4b9d5b0fd31f
glovas96 <alekceygolovko@gmail.com>
2025-01-03 14:34:03 +0300

    feat: add basic pages with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CityPage from "./pages/CityPage";
+import SearchPage from "./pages/SearchPage";
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/city/:id" element={<CityPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/CityPage.jsx
@@ -0,0 +1,5 @@
+function CityPage() {
+    return <h1>CityPage</h1>;
+}
+
+export default CityPage;
