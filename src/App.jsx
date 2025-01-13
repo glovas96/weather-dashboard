96b7566759f5e0eadda064eecd02ca40ab1f28d5
glovas96 <alekceygolovko@gmail.com>
2025-01-13 12:08:24 +0300

    feat: add minimal AppLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CityPage from "./pages/CityPage";
 import SearchPage from "./pages/SearchPage";
+import AppLayout from "./layouts/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/city/:id" element={<CityPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/city/:id" element={<CityPage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </AppLayout>
     </BrowserRouter>
   );
 }
---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,11 @@
+function AppLayout({ children }) {
+  return (
+    <div>
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default AppLayout;
