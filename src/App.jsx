efc4d0d214eafbd0e79a37cf1aa47bb2c7939dd4
glovas96 <alekceygolovko@gmail.com>
2025-01-19 16:24:08 +0300

    feat: integrate React Query provider into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HomePage from "./pages/HomePage";
 import CityPage from "./pages/CityPage";
 import SearchPage from "./pages/SearchPage";
 import AppLayout from "./layouts/AppLayout";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/city/:id" element={<CityPage />} />
-          <Route path="/search" element={<SearchPage />} />
-        </Routes>
-      </AppLayout>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/city/:id" element={<CityPage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Routes>
+        </AppLayout>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
